c25a905af240353d9a1f6c35be94c9a53e4a7d5b
myskite <125980622@qq.com>
2015-05-02 20:16:13 +0800

    v1

--- group6/12330242/MyHwManager/MyHwManager.js
+++ group6/12330242/MyHwManager/MyHwManager.js
@@ -2,110 +2,174 @@ Tasks = new Mongo.Collection("tasks");
 Dead = new Mongo.Collection("Dead");
 
 var hw = new FS.Collection("hw", {
-    stores: [new FS.Store.FileSystem("hw", {
-        path: "~/uploads"
-    })]
+  stores: [new FS.Store.FileSystem("hw", {
+    path: "~/uploads"
+  })]
 });
 
 if (Meteor.isClient) {
-    // Template.dlTemplate.helpers({
-    // 	deadl: function() {
-    // 		// alert(deadLine.find({}));
-    // 		// console.log(tojson(deadLine.find({})));
-    // 		return deadLine.find({});
-    // 	}
-    // });
-    Template.task.helpers({
+  // Template.dlTemplate.helpers({
+  // 	deadl: function() {
+  // 		// alert(deadLine.find({}));
+  // 		// console.log(tojson(deadLine.find({})));
+  // 		return deadLine.find({});
+  // 	}
+  // });
+  Template.task.helpers({
 
-        //
-        // isOwner: function() {
-        //     return this.owner === Meteor.userId();
-        // }
-    });
+    //
+    // isOwner: function() {
+    //     return this.owner === Meteor.userId();
+    // }
+  });
 
 
-    Template.body.helpers({
-        tasks: function() {
-            if (Meteor.user().username === "admin")
-              return Tasks.find({});
-            else
-              // return Tasks.findOne(Meteor.userId());
-              return Tasks.find({username : Meteor.user().username});
-        },
+  Template.body.helpers({
+    tasks: function() {
+      if (Meteor.user().username === "admin")
+        return Tasks.find({
+          username: {
+            $ne: "default"
+          }
+        });
+      else
+      // return Tasks.findOne(Meteor.userId());
+        return Tasks.find({
+        username: Meteor.user().username
+      });
+    },
 
-        isTeacher: function() {
-          return Meteor.user().username === "admin";
-        },
+    isTeacher: function() {
+      return Meteor.user().username === "admin";
+    },
 
-    });
+    init: function() {
+      // alert("flag");
+      console.log(Tasks.find().count());
+      if (Tasks.find().count() === 0) {
+        console.log("create collection");
+        Tasks.insert({
+          username: "default",
+          createdAt: "Yes",
+          owner: "default",
+          score: "No information ",
+          deadLine: "No information",
+          hwReq: "No information",
+          hwR: "No information"
+        });
+      }
+    },
+    getNum:function() {
+      return Tasks.find({username:"default"});
+    },
+  });
 
-    Template.body.events({
-        //upload homework
-        'change .upload': function(event, template) {
-            FS.Utility.eachFile(event, function(file) {
-                hw.insert(file, function(err, fileObj) {});
-            });
+  Template.body.events({
+    //upload homework
+    'change .upload': function(event, template) {
+      FS.Utility.eachFile(event, function(file) {
+        hw.insert(file, function(err, fileObj) {});
+      });
+      // console.log(Tasks.find({username : Meteor.user().username}).fetch().length)
+      if (Tasks.find({
+          username: Meteor.user().username
+        }).fetch().length === 0) {
+        Tasks.insert({
+          username: Meteor.user().username,
+          createdAt: "Yes",
+          owner: Meteor.userId(),
+          score: "No information",
+          deadLine: "No information",
+          hwReq: "No information",
+          hwR: "No information"
+        });
+      }
+    },
 
-            Tasks.insert({
-                username: Meteor.user().username,
-                createdAt: new Date(),
-                owner: Meteor.userId(),
-                score: "No information",
-                deadLine: "No information",
-                hwReq: "No information",
-                role: "student",
-                hwR : "No information"
-            });
-        },
+    'submit .new-deadline': function(event) {
+      var text = event.target.text.value;
+      Meteor.call("updateDl", text);
 
-        'submit .new-deadline': function(event) {
-            var text = event.target.text.value;
-            Meteor.call("updateDl", text);
+      event.target.text.value = "";
+      return false;
+    },
 
-            event.target.text.value = "";
-            return false;
-        },
+    'submit .new-require': function(event) {
+      var text = event.target.text.value;
+      Meteor.call("updateR", text);
 
-        'submit .new-require': function(event) {
-          var text = event.target.text.value;
-          Meteor.call("updateR", text);
+      event.target.text.value = "";
+      return false;
+    },
+    'submit .new-score': function(event) {
+      var text = event.target.text.value;
+      var textArray = text.split('|');
+      var _username = textArray[0];
+      var _score = textArray[1];
+      Meteor.call("updatesocre", _username, _score);
 
-          event.target.text.value = "";
-          return false;
-        }
-    });
+      event.target.text.value = "";
+      return false;
+    }
+  });
 
-    Accounts.ui.config({
-        passwordSignupFields: "USERNAME_ONLY"
-    });
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
 }
 
 Meteor.methods({
   //updateThe deadLine
-    updateDl: function(text) {
-        Tasks.update({}, {
-            $set: {
-                deadLine: text
-            }
-        }, {
-            multi: true
-        }, function(err, numaff, rawNum) {
-            if (err)
-                console.log(err);
-        });
-    },
+  updateDl: function(text) {
+    Tasks.update({}, {
+      $set: {
+        deadLine: text
+      }
+    }, {
+      multi: true
+    }, function(err, numaff, rawNum) {
+      if (err)
+        console.log(err);
+    });
+  },
 
-    updateR: function(text) {
-      Tasks.update({}, {
-        $set: {
-          hwR: text
-        }
-      }, {
-        multi: true
-      }, function(err, numaff, rawNum) {
-        if (err)
-          console.log(err);
-        });
-      },
+  updateR: function(text) {
+    Tasks.update({}, {
+      $set: {
+        hwR: text
+      }
+    }, {
+      multi: true
+    }, function(err, numaff, rawNum) {
+      if (err)
+        console.log(err);
+    });
+  },
+  updatesocre: function(_username, _score) {
+    Tasks.update({
+      username: _username
+    }, {
+      $set: {
+        score: _score
+      }
+    });
+  }
+
+  // updateHw: function(taskId) {
+  //   Tasks.update(taskId, {
+  //     $set: {
+  //       createdAt: new Date()
+  //     }},
+  //     {
+  //       multi: true
+  //     },
+  //     function(err, numAff, rawNum) {
+  //       if (err)
+  //         console.log(err);
+  //
+  //       console.log(numAff);
+  //       console.log(rawNum);
+  //     });
+  // }
 
 })
